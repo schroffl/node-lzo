69868c5f38def92d11f33bb315c3d26343eb38ad
schroffl <hannes.holzwarth@me.com>
2017-10-07 22:43:30 +0200

    Converted tabs to spaces: Same reason as #8365a22

--- test/test.js
+++ test/test.js
@@ -7,34 +7,34 @@ const lzo = require('../index');
 const expect = chai.expect;
 
 let data = crypto.randomBytes(100),
-		compressed;
+    compressed;
 
 describe('Compression', () => {
-	it('Should throw if nothing is passed', () => 
-		expect(() => lzo.compress()).to.throw() );
+  it('Should throw if nothing is passed', () => 
+    expect(() => lzo.compress()).to.throw() );
 
-	it('Should properly compress and not throw', () => {
-		expect(() => {
-			compressed = lzo.compress(data)
-		}).to.not.throw();
-	});
+  it('Should properly compress and not throw', () => {
+    expect(() => {
+      compressed = lzo.compress(data)
+    }).to.not.throw();
+  });
 });
 
 describe('Decompression', () => {
-	it('Should throw if nothing is passed', () => 
-		expect(() => lzo.decompress()).to.throw() );
+  it('Should throw if nothing is passed', () => 
+    expect(() => lzo.decompress()).to.throw() );
 
-	it('Should properly decompress and not throw', () =>
-		expect( data.compare(lzo.decompress(compressed)) ).to.equal(0) );
+  it('Should properly decompress and not throw', () =>
+    expect( data.compare(lzo.decompress(compressed)) ).to.equal(0) );
 });
 
 describe('Properties', () => {
-	it('Should have property \'version\'', () => 
-		expect(lzo).to.have.ownProperty('version') );
+  it('Should have property \'version\'', () => 
+    expect(lzo).to.have.ownProperty('version') );
 
-	it('Should have property \'versionDate\'', () => 
-		expect(lzo).to.have.ownProperty('versionDate') );
+  it('Should have property \'versionDate\'', () => 
+    expect(lzo).to.have.ownProperty('versionDate') );
 
-	it('Should have property \'errors\' (lzo error codes)', () =>
-		expect(lzo).to.have.ownProperty('errors') );
+  it('Should have property \'errors\' (lzo error codes)', () =>
+    expect(lzo).to.have.ownProperty('errors') );
 });
